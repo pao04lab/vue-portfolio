efa154421708f4e3ff516c23e62c0b9e39216e4b
pao04lab <127940379+pao04lab@users.noreply.github.com>
2023-05-22 15:57:59 +0200

    prova componenti

---
+++ vue-project/src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import PortfolioItem from './components/PortfolioItem.vue'
+</script>
+
+<template>
+  <header class="mb-4">
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div class="wrapper">
+      <HelloWorld msg="custom message" />
+    </div>
+  </header>
+
+  <main>
+<div class="row">
+  <div class="col">
+    <PortfolioItem 
+      title="ciao mondo"
+      abstract="Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore."
+      imageUrl="/images/pere.jpg"
+       />
+  </div>
+  <div class="col">
+    <PortfolioItem 
+      title="Ola"
+      abstract="Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore. magna aliqua."
+      imageUrl="https://source.unsplash.com/random/?green" />
+  </div>
+</div>
+  </main>
+</template>
+
+<style scoped>
+
+
+header {
+  line-height: 1.5;
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ vue-project/src/components/PortfolioItem.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  abstract: String,
+  color: String,
+  imageUrl: String
+})
+</script>
+
+<template>
+  <div class="custom-component"></div>
+  <div class="title text-center"  :style="{backgroundColor: color}">
+    <img :src="imageUrl" alt="" class="image image-fuid">
+    <h2>{{ title }}</h2>
+    <p class="abstract">
+      {{ abstract }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+  .title {
+    font-size: 1em;
+  }
+
+ .image {
+    width: 100%;
+    height: 100%;
+  }
+
+ /* .custom-component {
+    background-color: v-bind(color);
+  }*/
+</style>
